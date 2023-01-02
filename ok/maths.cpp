20e6bf3b21b9ad26e01e51b5b0ddb61f35077e09
Abhi <abhishekbiradar753@gmail.com>
2023-01-02 18:29:20 +0530

    Added more cpp

---
+++ ok/maths.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    int b=1;
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<a*b<<endl;
+    cout<<a%b<<endl;
+    cout<<++a<<endl;
+    cout<<a+b<<endl;
+    return 0;
+    
+}
